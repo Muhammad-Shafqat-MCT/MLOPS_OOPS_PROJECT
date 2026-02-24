a11b597ccfb2844c7466e580515cb8438aa7c7b0
Muhammad_Shafqat <muhammadshafqat7866@gmail.com>
2026-02-24 07:22:51 +0500

    Created Attribute outside the class

--- oops1.py
+++ oops1.py
@@ -1,22 +1,30 @@
 class Employee:
-    #Special Function/Method, Megic Metho/Dunder Method- Constructor
+    #Special Function/Method, Megic Method/Dunder Method- Constructor
     def __init__(self):
-        print("Strating executing attributes/data")
+        # print(id(self))
+        # print("Strating executing attributes/data")
         self.id= 123
         self.salary= 50000
         self.designation= "Software Development Engineer"
-        print("Attribute/data have been initiated")
+        # print("Attribute/data have been initiated")
 
     def travel(self,destination):
         print("This travel method was called Manually")
         print(f"Employee is now travelling to {destination}")    
 #Creating an object/instance of the class
 sam=Employee()
+#creating attribute outside the class:
+sam.name="Sami"
+print(sam.name)
+# print(id(sam))
+
+# ali=Employee()
+# print(id(ali))
 
 #printing the attributes
 #print(sam.id)
 
 #Calling a Method
-sam.travel("USA")
+# sam.travel("USA")
 
-print(type(sam))
+# print(type(sam))
--- rough.py
+++ rough.py
@@ -7,3 +7,19 @@ print(type(my_str))
 print(type(my_int))
 my_str=my_str.capitalize()
 print(my_str)
+
+from oops_proj import chatbook
+# user1=chatbook()
+
+# #Function Vs Method Below
+# lst=[1,2,3]
+
+# # function
+
+# a1=len(lst)
+# print(a1)
+
+# #Method is written under the class
+# #This is how we called a method
+# user1=chatbook()
+# user1.sendmsg()
